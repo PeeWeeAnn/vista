6fa16008efb0ebfe55e867f6d121eeda991d5d8a
Anna Kautts <annakautts@gmail.com>
2024-03-12 21:33:45 +0500

    redirect after time

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    window.setTimeout(() => {
+      navigate("/auth");
+    }, 3000)
+  })
+
   return (
     <>
-    
+     <h1>Welcome</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from './App';
+import Auth from './pages/Auth';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
